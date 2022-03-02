9d8bed887e6b1e070bce795c73aebbea3e44fab3
nanamulyana123 <nanamlpermana2@gmail.com>
2022-03-02 16:02:01 +0700

    insert first project

---
+++ PasswordmanagerNana/src/main/java/com/nana/passwordmanagernana/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.nana.passwordmanagernana;
+
+import View.JFLoginScreen;
+
+/**
+ *
+ * @author Nana
+ */
+public class Main {
+        /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //System.out.println("Hello Word");
+       
+        JFLoginScreen jfLoginScreen = new JFLoginScreen();
+        jfLoginScreen.setLocationRelativeTo(null); //
+        jfLoginScreen.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
